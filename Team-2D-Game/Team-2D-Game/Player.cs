07ebdb2f65658a136db22612f5fc66e749d10e0f
Bill Conn <b_conn@hotmail.com>
2013-01-29 02:45:36 -0600

    added unfinished player class

---
+++ Team-2D-Game/Team-2D-Game/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Team_2D_Game
+{
+    class Player
+    {
+        //All variables for the player
+        int health = 100; //start with 100 health
+        Vector2 postition;
+        Vector2 hatOrigin;
+        Vector2 staffOrigin;
+    }
+}
